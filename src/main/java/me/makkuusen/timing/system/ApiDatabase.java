d21708d3bd2ba2d3b7b38fb922bbe43da4e2e35c
Makkuusen <makkuusen@live.se>
2022-07-09 17:46:14 +0200

    Create tables if not exists

--- src/main/java/me/makkuusen/timing/system/ApiDatabase.java
+++ src/main/java/me/makkuusen/timing/system/ApiDatabase.java
@@ -27,24 +27,8 @@ public class ApiDatabase {
         dataSource.setPassword( plugin.getConfig().getString("sql.password"));
 
         ApiDatabase.dataSource = dataSource;
+        return createTables();
 
-        try
-        {
-            Connection connection = ApiDatabase.getConnection();
-            Statement statement = connection.createStatement();
-
-
-            statement.close();
-            connection.close();
-
-            return true;
-        }
-
-        catch (SQLException exception)
-        {
-            plugin.getLogger().warning(exception.getMessage());
-            return false;
-        }
     }
     static boolean synchronize() {
         try {
@@ -233,5 +217,73 @@ public class ApiDatabase {
     public static Collection<TSPlayer> getPlayers()
     {
         return plugin.players.values();
+    }
+
+    public static boolean createTables(){
+        try {
+
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `players` (\n" +
+                    "  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',\n" +
+                    "  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `dateJoin` bigint(30) DEFAULT NULL,\n" +
+                    "  `dateNameChange` bigint(30) DEFAULT NULL,\n" +
+                    "  `dateNameCheck` bigint(30) DEFAULT NULL,\n" +
+                    "  `dateSeen` bigint(30) DEFAULT NULL,\n" +
+                    "  PRIMARY KEY (`uuid`)\n" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `tracks` (\n" +
+                    "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,\n" +
+                    "  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `dateCreated` bigint(30) DEFAULT NULL,\n" +
+                    "  `guiItem` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `spawn` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `leaderboard` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `type` longblob NOT NULL,\n" +
+                    "  `mode` longblob DEFAULT NULL,\n" +
+                    "  `toggleOpen` tinyint(1) NOT NULL,\n" +
+                    "  `toggleGovernment` tinyint(1) NOT NULL,\n" +
+                    "  `options` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,\n" +
+                    "  `isRemoved` tinyint(1) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`)\n" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `tracksFinishes` (\n" +
+                    "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `trackId` int(11) NOT NULL,\n" +
+                    "  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `date` bigint(30) NOT NULL,\n" +
+                    "  `time` int(11) NOT NULL,\n" +
+                    "  `isRemoved` tinyint(1) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`)\n" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `tracksRegions` (\n" +
+                    "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+                    "  `trackId` int(11) NOT NULL,\n" +
+                    "  `regionIndex` int(11) DEFAULT NULL,\n" +
+                    "  `regionType` longblob DEFAULT NULL,\n" +
+                    "  `minP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,\n" +
+                    "  `maxP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,\n" +
+                    "  `spawn` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,\n" +
+                    "  `isRemoved` tinyint(1) NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`)\n" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+
+            statement.close();
+            connection.close();
+            return true;
+
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+
+
     }
 }
