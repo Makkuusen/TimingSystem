52821e753cc1ede2c5fd2c3dcf121ffc4385fb81
Pigalala <w8888mc@gmail.com>
2022-09-16 22:21:21 +0100

    Check for player's time

--- src/main/java/me/makkuusen/timing/system/CommandTimeTrial.java
+++ src/main/java/me/makkuusen/timing/system/CommandTimeTrial.java
@@ -84,9 +84,13 @@ public class CommandTimeTrial extends BaseCommand {
             player.sendMessage("§cWorld is not loaded!");
             return;
         }
-        player.sendMessage(plugin.getLocalizedMessage(player, "messages.timer.randomTrack", "%track%", t.getDisplayName(),
-                "%pos%", String.valueOf(t.getPlayerTopListPosition(Database.getPlayer(player.getUniqueId())))
-        ));
+        if(t.getPlayerTopListPosition(Database.getPlayer(player.getUniqueId())) != -1){
+            player.sendMessage(plugin.getLocalizedMessage(player, "messages.timer.randomTrack", "%track%", t.getDisplayName(),
+                    "%pos%", String.valueOf(t.getPlayerTopListPosition(Database.getPlayer(player.getUniqueId())))
+            ));
+        } else {
+            player.sendMessage(plugin.getLocalizedMessage(player, "messages.timer.randomTrackNoPos", "%track%", t.getDisplayName()));
+        }
 
         t.getSpawnLocation().setPitch(player.getLocation().getPitch());
         player.teleport(t.getSpawnLocation());
