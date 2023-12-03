5cc3b583ec5e6e59457497f59066d76d7940ca6c
o7Moon <91579431+o7Moon@users.noreply.github.com>
2023-12-03 06:21:52 -0600

    tag existing modes with BROKEN_SLIME_ and new modes with slime slipperiness fix are added (#322)

--- src/main/java/me/makkuusen/timing/system/boatutils/BoatUtilsMode.java
+++ src/main/java/me/makkuusen/timing/system/boatutils/BoatUtilsMode.java
@@ -2,14 +2,22 @@ package me.makkuusen.timing.system.boatutils;
 
 public enum BoatUtilsMode {
     VANILLA(-1, 0),
-    RALLY(0, 0),
-    RALLY_BLUE(1, 0),
-    BA_NOFD(2, 0),
-    PARKOUR(3, 0),
-    BA_BLUE_NOFD(4,2),
-    PARKOUR_BLUE(5, 4),
-    BA(6, 4),
-    BA_BLUE(7, 4);
+    BROKEN_SLIME_RALLY(0, 0),
+    BROKEN_SLIME_RALLY_BLUE(1, 0),
+    BROKEN_SLIME_BA_NOFD(2, 0),
+    BROKEN_SLIME_PARKOUR(3, 0),
+    BROKEN_SLIME_BA_BLUE_NOFD(4,2),
+    BROKEN_SLIME_PARKOUR_BLUE(5, 4),
+    BROKEN_SLIME_BA(6, 4),
+    BROKEN_SLIME_BA_BLUE(7, 4),
+    RALLY(8, 5),
+    RALLY_BLUE(9, 5),
+    BA_NOFD(10, 5),
+    PARKOUR(11, 5),
+    BA_BLUE_NOFD(12, 5),
+    PARKOUR_BLUE(13, 5),
+    BA(14, 5),
+    BA_BLUE(15, 5);
 
     private final short id;
     private final short version;
