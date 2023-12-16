ed3c9aa82b8274c89c9c79c7ab990108eb1a3aa1
Makkuusen <makkuusen@live.se>
2023-12-16 11:53:30 +0100

    Fix problem causing finish_tp to not work

--- src/main/java/me/makkuusen/timing/system/heat/FinalHeat.java
+++ src/main/java/me/makkuusen/timing/system/heat/FinalHeat.java
@@ -33,8 +33,8 @@ public class FinalHeat {
 
         Bukkit.getServer().getScheduler().runTaskAsynchronously(TimingSystem.getPlugin(), () -> {
             driver.getHeat().getEvent().getTrack().getTrackLocations().getFinishTp(driver.getPosition()).ifPresentOrElse(
-                    loc -> driver.getTPlayer().getPlayer().teleport(loc),
-                    () -> driver.getHeat().getEvent().getTrack().getTrackLocations().getFinishTp().ifPresent(loc -> driver.getTPlayer().getPlayer().teleport(loc))
+                    loc -> driver.getTPlayer().getPlayer().teleportAsync(loc),
+                    () -> driver.getHeat().getEvent().getTrack().getTrackLocations().getFinishTp().ifPresent(loc -> driver.getTPlayer().getPlayer().teleportAsync(loc))
             );
         });
     }
