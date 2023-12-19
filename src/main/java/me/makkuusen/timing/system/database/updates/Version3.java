ba9a0443f9a1e157a448cd4829c50cf01c2f6eb5
Makkuusen <makkuusen@live.se>
2023-12-19 21:36:58 +0100

    Remove async db upgrade

--- src/main/java/me/makkuusen/timing/system/database/updates/Version3.java
+++ src/main/java/me/makkuusen/timing/system/database/updates/Version3.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class Version3 {
 
-    public static void updateMySQL() {
-        DB.executeUpdateAsync("ALTER TABLE `ts_players` ADD `shortName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL after `name`;");
-        DB.executeUpdateAsync("ALTER TABLE `ts_tracks` ADD `timeTrial` tinyint(1) NOT NULL DEFAULT 1 after `type`;");
-        DB.executeUpdateAsync("ALTER TABLE `ts_tracks` DROP COLUMN `mode`;");
-        DB.executeUpdateAsync("UPDATE `ts_regions` SET `regionIndex` = 1 WHERE `regionType` = 'LAGSTART' OR `regionType` = 'LAGEND';");
+    public static void updateMySQL() throws SQLException{
+        DB.executeUpdate("ALTER TABLE `ts_players` ADD `shortName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL after `name`;");
+        DB.executeUpdate("ALTER TABLE `ts_tracks` ADD `timeTrial` tinyint(1) NOT NULL DEFAULT 1 after `type`;");
+        DB.executeUpdate("ALTER TABLE `ts_tracks` DROP COLUMN `mode`;");
+        DB.executeUpdate("UPDATE `ts_regions` SET `regionIndex` = 1 WHERE `regionType` = 'LAGSTART' OR `regionType` = 'LAGEND';");
     }
 
     public static void updateSQLite() throws SQLException {
