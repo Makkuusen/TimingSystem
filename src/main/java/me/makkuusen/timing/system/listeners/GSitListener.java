86f76c8a283020dad2dc2f9ea2a982c0eba81146
Makkuusen <makkuusen@live.se>
2024-01-24 18:23:30 +0100

    Fix GSitListener

--- src/main/java/me/makkuusen/timing/system/listeners/GSitListener.java
+++ src/main/java/me/makkuusen/timing/system/listeners/GSitListener.java
@@ -1,17 +1,20 @@
 package me.makkuusen.timing.system.listeners;
 
 
-import dev.geco.gsit.api.event.PrePlayerPlayerSitEvent;
+import dev.geco.gsit.api.event.PreEntitySitEvent;
 import me.makkuusen.timing.system.timetrial.TimeTrialController;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class GSitListener implements Listener {
 
     @EventHandler
-    public void onGSitEvent(PrePlayerPlayerSitEvent event) {
-        if (TimeTrialController.timeTrials.containsKey(event.getPlayer().getUniqueId())) {
-            event.setCancelled(true);
+    public void onGSitEvent(PreEntitySitEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (TimeTrialController.timeTrials.containsKey(player.getUniqueId())){
+                event.setCancelled(true);
+            }
         }
     }
 }
